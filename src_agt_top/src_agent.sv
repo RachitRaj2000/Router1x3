26144c8799b1cb4b7d785dd733a7c5d1802389e7
RachitRaj2000 <168286367+RachitRaj2000@users.noreply.github.com>
2024-10-11 21:44:07 +0530

    Update src_agent.sv

--- src_agt_top/src_agent.sv
+++ src_agt_top/src_agent.sv
@@ -1,21 +1,20 @@
-
-	class src_agent extends uvm_agent;
-	`uvm_component_utils(src_agent)
-        src_config src_cfg;
-	src_monitor monh;
-	src_sequencer seqrh;
-	src_driver drvh;
-  extern function new(string name = "src_agent", uvm_component parent = null);
-  extern function void build_phase(uvm_phase phase);
-  extern function void connect_phase(uvm_phase phase);
+class src_agent extends uvm_agent;
+`uvm_component_utils(src_agent)
+src_config src_cfg;
+src_monitor monh;
+src_sequencer seqrh;
+src_driver drvh;
+extern function new(string name = "src_agent", uvm_component parent = null);
+extern function void build_phase(uvm_phase phase);
+extern function void connect_phase(uvm_phase phase);
 
 endclass : src_agent
 
-       function src_agent::new(string name = "src_agent", 
+function src_agent::new(string name = "src_agent", 
                                uvm_component parent = null);
          super.new(name, parent);
-       endfunction
-	function void src_agent::build_phase(uvm_phase phase);
+endfunction
+function void src_agent::build_phase(uvm_phase phase);
 		super.build_phase(phase);
 	  if(!uvm_config_db #(src_config)::get(this,"","src_config",src_cfg)) begin
 		`uvm_fatal("CONFIG","cannot get() m_cfg from uvm_config_db. Have you set() it?")
@@ -26,13 +25,13 @@ endclass : src_agent
 		drvh=src_driver::type_id::create("drvh",this);
 		seqrh=src_sequencer::type_id::create("seqrh",this);
 		end
-	endfunction
-	function void src_agent::connect_phase(uvm_phase phase);
+endfunction
+function void src_agent::connect_phase(uvm_phase phase);
 		if(src_cfg.is_active==UVM_ACTIVE)
 		begin
 		drvh.seq_item_port.connect(seqrh.seq_item_export);
   		end
-	endfunction
+endfunction
    
 
    
