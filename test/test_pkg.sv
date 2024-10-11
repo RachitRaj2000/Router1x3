5ab2981386ceb194e06a5d1aa4f54f208c1d6bfa
RachitRaj2000 <168286367+RachitRaj2000@users.noreply.github.com>
2024-10-11 22:03:27 +0530

    Update test_pkg.sv

--- test/test_pkg.sv
+++ test/test_pkg.sv
@@ -1,31 +1,7 @@
-/************************************************************************
-  
-Copyright 2019 - Maven Silicon Softech Pvt Ltd.  
-  
-www.maven-silicon.com 
-  
-All Rights Reserved. 
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd. 
-It is not to be shared with or used by any third parties who have not enrolled for our paid 
-training courses or received any written authorization from Maven Silicon.
-  
-Filename:       ram_test_pkg.sv
-  
-Author Name:    Putta Satish
-
-Support e-mail: For any queries, reach out to us on "techsupport_vm@maven-silicon.com" 
-
-Version:	1.0
-
-*/
 package test_pkg;
 
   typedef enum{UVM_PASSIVE, UVM_ACTIVE} uvm_active_passive_enum;
-
-  // Importing UVM package
   import uvm_pkg::*;
-  
-  // Including macros and other necessary files
   `include "uvm_macros.svh"
   `include "src_xtn.sv"
   `include "dest_xtn.sv"
